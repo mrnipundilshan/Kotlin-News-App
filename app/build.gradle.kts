3abcc53e6e33b2de484b4bfbd9d53bbd132f291a
Nipun Dilshan <139701467+mrnipundilshan@users.noreply.github.com>
2026-09-22 09:21:53 +0530

    Add splash screen support using AndroidX SplashScreen API
    - Add `androidx.core:core-splashscreen` dependency in Gradle
    - Define splash screen vector drawable and theme styles for light and dark modes
    - Update application and `MainActivity` themes to `App.Starting.Theme` in `AndroidManifest.xml`
    - Call `installSplashScreen()` in `MainActivity`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,10 +45,12 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     testImplementation(libs.junit)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
+
+    //Splash Api
+    implementation(libs.androidx.core.splashscreen)
 }
